fad5068c24f6498e0915825f8cef04bb9f6c6884
Lucas Vieira <lucas_99silva@hotmail.com>
2024-08-13 11:35:49 -0300

    style: add theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme"
+import { GlobalStyles } from "./styles/global"
+
 export function App() {
-  return <h1>Hello World!!</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World!!</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,38 @@
+export const defaultTheme = {
+  colors: {
+    "yellow-dark": "#C47F17",
+    "yellow": "#DBAC2C",
+    "yellow-light": "#F1E9C9",
+    "purple-dark": "#4B2995",
+    "purple": "#8047F8",
+    "purple-light": "#EBE5F9",
+    "base-title": "#272221",
+    "base-subtitle": "#403937",
+    "base-text": "#574F4D",
+    "base-label": "#8D8686",
+    "base-hover": "#D7D5D5",
+    "base-button": "#E6E5E5",
+    "base-input": "#EDEDED",
+    "base-card": "#F3F2F2",
+    background: "#FAFAFA",
+    white: "#FFFFFF"
+  },
+  fonts: {
+    "baloo-2": {
+      "title-xs": 18,
+      "title-s": 20,
+      "title-m": 24,
+      "title-l": 32,
+      "title-xl": 48
+    },
+    roboto: {
+      "text-xs": 12,
+      "text-s": 14,
+      "text-m": 16,
+      "text-l": 20,
+      "text-tag": 10,
+      "text-button-G": 14,
+      "text-button-M": 12
+    }
+  }
+}
